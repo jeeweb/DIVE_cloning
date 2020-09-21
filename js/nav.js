68b4c974ebfa4f3435439bcc424a7926ee9374ff
jeeweb <jee.vvvv@gmail.com>
2020-09-21 23:30:13 +0900

    mobile 메뉴(nav) 작업

---
+++ js/nav.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const openNavBtn = document.querySelector('.btn-nav'),
+	navMobile = document.querySelector('.nav-mobile'),
+	closeNavBtn = navMobile.querySelector('.btn__close'),
+	navMobileMenu = navMobile.querySelectorAll('.lst-gnb__item'),
+	navMobileSub = navMobile.querySelectorAll('.lst-gnb-sub');
+
+openNavBtn.addEventListener('click', () => navMobile.classList.add('nav-mobile--active'));
+
+closeNavBtn.addEventListener('click', () => navMobile.classList.remove('nav-mobile--active'));
+
+for (let i = 0; i < navMobileMenu.length; i++) {
+	navMobileMenu[i].addEventListener('click', (e) => {
+		let target = e.currentTarget;
+		console.log(target.childNode)
+		// if(target.childNode)
+	})
+
+}
