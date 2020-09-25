344e5090896b9a976e83378d55c196a223c32d16
jeeweb <jee.vvvv@gmail.com>
2020-09-25 13:21:29 +0900

    design library 반응형작업 (+컨텐츠수정)

--- js/space.js
+++ js/space.js
@@ -1,19 +1,34 @@
 'use strict';
 const header = document.querySelector('#header');
 let yOffset = 0; 
+let winWidth ;
 
+function checkWinWidth() {
+	winWidth = window.innerWidth;
+	if (winWidth <= 768) {
+		header.classList.add('bg-white');
+	} else {
+		changeBgHeader();
+	};
+	console.log(winWidth);
+}
 // scroll 시 header 색상 변경
-function chageBgHeader() {
+function changeBgHeader() {
     //console.log(yOffset)
     if (yOffset > 0) {
       header.classList.add('bg-white');
     } else if (yOffset === 0) {
       header.classList.remove('bg-white');
     }
-  }
+	}
+
+	window.addEventListener('load', checkWinWidth());
+	window.onresize = checkWinWidth; 
   
   window.addEventListener('scroll', () => {
     yOffset = window.pageYOffset
     //console.log(window.pageYOffset);
-    chageBgHeader();
-  })
+		if (winWidth > 768) changeBgHeader();
+		
+	})
+	
